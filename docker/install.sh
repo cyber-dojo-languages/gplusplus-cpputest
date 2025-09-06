0b2b99ad7d3f243e4eab1e491f2aa7c7dac19f24
JonJagger <jon@kosli.com>
2025-09-06 17:27:46 +0100

    Switch to github workflow, g++ 15.1.0, cpputest 4.0

--- docker/install.sh
+++ docker/install.sh
@@ -1,18 +1,13 @@
-#!/bin/bash -Eeu
+#!/usr/bin/env bash
+set -Eeu
 
-apk add --no-cache gcc g++ musl-dev make
 apk add --no-cache autoconf automake git libtool
 
-cd /
+git clone https://github.com/cpputest/cpputest.git
+cd cpputest
 
-wget https://github.com/cpputest/cpputest/releases/download/latest-passing-build/cpputest-latest.tar.gz
-tar -xf ./cpputest-latest.tar.gz
-rm ./cpputest-latest.tar.gz
-mv cpputest-latest cpputest
-
-cd cpputest/
 autoreconf . -i
 ./configure
-make
+make tdd
 
 apk del git
